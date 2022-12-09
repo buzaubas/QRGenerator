c9eb11937682b813403752f20d4f7cca6842533f
Beks_jf39@student.itstep.kz <Beks_jf39@student.itstep.kz>
2022-12-09 21:55:04 +0600

    Добавьте файлы проекта.

---
+++ QRGenerator/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QRGenerator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerateQrCode_Click(object sender, RoutedEventArgs e)
+        {
+            //getting size
+            //var rb = rdSize.Children;
+            //int size = 0;
+            //foreach (var item in rdSize.Children)
+            //{
+            //    RadioButton rb = (RadioButton)item;
+
+            //    if ((bool)rb.IsChecked) //без bool не уверен что там не нулл
+            //    {
+            //        size = Convert.ToInt32(rb.Content);
+            //        break;
+            //    }
+            //}
+
+            RadioButton rb_ = rdSize.Children.Cast<RadioButton>().FirstOrDefault(item => (bool)item.IsChecked); //LINQ
+
+            var restClient = new RestClient("http://qrcoder.ru/code");
+
+            var request = new RestRequest(String.Format("?{0}&{1}&0", tbxQrText.Text, rb_), Method.Get);
+
+            RestResponse response = restClient.Execute(request);    
+            
+            var img = response.RawBytes;
+
+            WindowQrCodeImage wqci = new WindowQrCodeImage(img, 200, 200);
+            wqci.Show();
+        }
+    }
+}
---
+++ QRGenerator/WindowQrCodeImage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QRGenerator
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowQrCodeImage.xaml
+    /// </summary>
+    public partial class WindowQrCodeImage : Window
+    {
+        public WindowQrCodeImage(byte[] buffer, int height, int width)
+        {
+            InitializeComponent();
+
+            GetImage(buffer, qrImage);
+            this.Height = height;
+            this.Width = width;
+        }
+
+        private void GetImage(byte[] rawBytes, Image img)
+        {
+            byte[] imageBytes = rawBytes;
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = new MemoryStream(imageBytes);
+            bitmapImage.CreateOptions = BitmapCreateOptions.None;
+            bitmapImage.CacheOption = BitmapCacheOption.Default;
+            bitmapImage.EndInit();
+
+            
+            this.Dispatcher.Invoke((Action)(() => { img.Source = bitmapImage; }));
+        }
+    }
+}
